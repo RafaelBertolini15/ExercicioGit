2ed4138568dd0fc1a678e596caf7d4100b9b6ab3
brtl15 <rafaelhbertolini@gmail.com>
2025-08-06 00:12:10 -0300

    adicionando ola mundo

--- GitExercicio/src/helloWorld/OlaMundo.java
+++ GitExercicio/src/helloWorld/OlaMundo.java
@@ -1,10 +1,10 @@
 package helloWorld;
 
-public class HelloWorld {
+public class OlaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println("Ola mundo!");
 
 	}
 
